d81cb09fe6cff5b9ee4b0e125ad6f171edbaca73
liushuai-fotoable <liushuai@fotoable.com>
2019-08-28 16:48:40 +0800

    test

---
+++ FATSqlData.framework/Headers/FATFMDB.h
@@ -0,0 +1,5 @@
+#import "FATDatabase.h"
+#import "FATResultSet.h"
+#import "FMDatabaseAdditions.h"
+#import "FATDatabaseQueue.h"
+#import "FATDatabasePool.h"
---
+++ FATSqlData.framework/Headers/FATSqlData.h
@@ -0,0 +1,22 @@
+//
+//  FATSqlData.h
+//  FATSqlData
+//
+//  Created by 傅瑶 on 2019/7/11.
+//  Copyright © 2019 傅瑶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FATSqlData.
+FOUNDATION_EXPORT double FATSqlDataVersionNumber;
+
+//! Project version string for FATSqlData.
+FOUNDATION_EXPORT const unsigned char FATSqlDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FATSqlData/PublicHeader.h>
+
+#import <FATSqlData/FATFMDB.h>
+#import <FATSqlData/FATDBHelper.h>
+#import <FATSqlData/FATDBModel.h>
+
